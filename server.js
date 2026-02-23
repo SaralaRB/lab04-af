beb17e891f3b0ecff7f3898b0dfb4e8a0fdc12a7
SaralaRB <it23289420@my.sliit.lk>
2026-02-23 22:35:52 +0530

    Added file read/write feature

---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require("http");
+
+http.createServer(function (req, res) {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write("Hello World!");
+  res.end();
+}).listen(3000);
+
+console.log("Server running on http://localhost:3000");
